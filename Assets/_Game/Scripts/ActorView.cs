632417540da91a176031c49e36a7e42c9d2d5bd1
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2021-01-23 16:28:31 -0700

    wip - working on ActorViewManager and ActorView

---
+++ Assets/_Game/Scripts/ActorView.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Tofunaut.AnPrimTheGame
+{
+    public class ActorView : MonoBehaviour
+    {
+        public ulong Entity { get; private set; }
+
+        public void Initialize(ulong entity)
+        {
+            Entity = entity;
+        }
+    }
+}
